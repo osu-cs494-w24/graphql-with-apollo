35896125977ff3c0905bad42114e6d4b42e5144d
Rob Hess <hessro@oregonstate.edu>
2024-03-06 15:31:24 -0800

    Add code from 3/6 lecture.

--- testApollo.js
+++ testApollo.js
@@ -1,11 +1,14 @@
-import { ApolloClient, gql, InMemoryCache } from '@apollo/client'
-import 'dotenv'
+import { ApolloClient, gql, InMemoryCache } from '@apollo/client/core/core.cjs'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: ".env.local" })
 
 const client = new ApolloClient({
     uri: "https://api.github.com/graphql",
     headers: {
         Authorization: `Bearer ${process.env.VITE_GITHUB_TOKEN}`
-    }
+    },
+    cache: new InMemoryCache()
 })
 
 const query = gql`
